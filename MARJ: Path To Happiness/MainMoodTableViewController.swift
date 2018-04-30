2e86753a9adadcb6b900aad2702e9a44736d1d45
Michael <mpmurphy1432@gmail.com>
2018-04-30 11:41:56 -0500

    Nothing

--- MARJ: Path To Happiness/MainMoodTableViewController.swift
+++ MARJ: Path To Happiness/MainMoodTableViewController.swift
@@ -13,10 +13,10 @@ class MainMoodTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
+     
         // self.clearsSelectionOnViewWillAppear = false
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+       
          self.navigationItem.leftBarButtonItem = editButtonItem
     }
 
