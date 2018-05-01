9be120626badae29c1036797de69cfd97e23d8b5
Michael <mpmurphy1432@gmail.com>
2018-05-01 11:06:22 -0500

    Commit

--- MARJ: Path To Happiness/MainMoodTableViewController.swift
+++ MARJ: Path To Happiness/MainMoodTableViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class MainMoodTableViewController: UITableViewController {
 
+    var moods: [Mood]!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -32,24 +36,24 @@ class MainMoodTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+
+        return 1
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mainCellId", for: indexPath) as! mainMoodCell
 
         // Configure the cell...
 
         return cell
     }
-    */
+ 
 
     /*
     // Override to support conditional editing of the table view.
